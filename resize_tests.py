f3a0e8d27778404bd0e01614022e2af7521b1ee7
Unknown15082 <trangiahuy15082006@gmail.com>
2022-07-22 11:56:08 +0700

    Resize tests

---
+++ resize_tests.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import os
+for x in os.listdir("./test/"):
+    for y in os.listdir("./test/" + x):
+        img = Image.open("./test/" + x + "/" + y)
+        img = img.resize((64, 64))
+        img.save("./DataModified/Test/%s.png" % (x + "_" + y[:-4]))
+ff.close()
