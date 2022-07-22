2576c95ce2849aa3dc72faf9246d7b2a71ad6efc
Unknown15082 <trangiahuy15082006@gmail.com>
2022-07-22 10:29:12 +0700

    Resize files

---
+++ resize_files.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import os
+ff = open("./DataModified/modified_labels.csv", "w")
+ff.write("Frame,Label\n")
+for x in os.listdir("./image/"):
+    f = [x + "_" + z for z in open("./label/" + x + ".csv", "r").read().strip().split("\n")[1:]]
+    ff.write("\n".join(f) + "\n")
+    for y in os.listdir("./image/" + x):
+        img = Image.open("./image/" + x + "/" + y)
+        img = img.resize((64, 64))
+        img.save("./DataModified/Train/%s.png" % (x + "_" + y[:-4]))
+ff.close()
